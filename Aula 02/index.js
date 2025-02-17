5120395f510bfcadc421b7676bb943733405d614
João Pedro <senac.jotapedro@gmail.com>
2025-02-17 17:31:24 -0300

    add: concluido aula 1702

--- Aula 02/index.js
+++ Aula 02/index.js
@@ -1,6 +1,9 @@
 // Importando com (ESM)
-import express from 'express';
-import dotenv from 'dotenv';
+// import express from 'express';
+// import dotenv from 'dotenv';
+
+const express = require('express');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
@@ -12,15 +15,120 @@ app.use(express.json());
 const bancoDados = [];
 
 app.get('/produtos', (requisicao, resposta) => {
-  resposta.json(bancoDados);
+  try {
+    if (bancoDados.length === 0) {
+      return resposta.status(200).json({ mensagem: "Banco de dados vazio" });
+    }
+
+    resposta.status(200).json(bancoDados);
+
+  } catch (error) {
+    resposta.status(500).json(
+      {
+        msg: "Erro ao buscar produtos",
+        erro: error.mensagem
+      }
+    );
+  }
+
 });
 
+
+// outro get
+
+app.get('/produtos/:id', (requisicao, resposta) => {
+  try {
+    const id = requisicao.params.id;
+    const produto = bancoDados.find(elemento => elemento.id === id);
+    if (!produto) {
+      return resposta.status(404).json({mensagem: "Produto não encontrado"})
+    }
+
+    resposta.status(200).json(produto);
+  } catch (error) {
+    resposta.status(500).json(
+      {
+        msg: "Erro ao buscar produto",
+        erro: error.mensagem
+      }
+    );
+  }
+})
+
 app.post('/produtos', (requisicao, resposta) => {
-  const { id, nome, preco } = requisicao.body;
-  const novoProduto = { id, nome, preco };
-  bancoDados.push(novoProduto);
-  resposta.status(201).json({ mensagem: "Produto criado com sucesso" });
+  try {
+    const { id, nome, preco } = requisicao.body;
+
+    if (!id || !nome || !preco) {
+      return resposta.status(200).json({ mensagem: "todos os dados devem ser informados" })
+    }
+
+    const novoProduto = { id, nome, preco };
+    bancoDados.push(novoProduto);
+    resposta.status(201).json({ mensagem: "Produto criado com sucesso" });
+  } catch (error) {
+    resposta.status(500).json(
+      {
+        msg: "Erro ao cadastrar produtos",
+        erro: error.mensagem
+      }
+    );
+  }
 });
+
+app.put('/produtos/:id', (requisicao, resposta) => {
+
+  try {
+
+    const id = requisicao.params.id;
+  if(!id){
+    return resposta.status(404).json({mensagem: "Informe um parâmetro"})
+  }
+  const produto = bancoDados.find((produtos) => produtos.id === id);
+  if(!produto){
+    return resposta.status(404).json({mensagem: "Produto não encontrado"})
+  }
+
+  const {novoNome, novoPreco} = requisicao.body;
+
+  if (produto) {
+      produto.nome = novoNome
+      produto.preco = novoPreco
+  }
+
+  resposta.status(200).json({msg: "Produto atualizado com sucesso"});
+    
+  } catch (error) {
+    resposta.status(500).json(
+      {
+        msg: "Erro ao atualizar produtos",
+        erro: error.mensagem
+      }
+    );
+  }
+  
+
+})
+
+app.delete('/produtos/:id', (requisicao, resposta) => {
+  try {
+    const id = requisicao.params.id;
+    const index = bancoDados.findIndex(elemento => elemento.id === id);
+    if (index === -1) {
+      return resposta.status(404).json({mensagem: "Produto não encontrado"});
+    }
+
+    bancoDados.splice(index, 1);
+    resposta.status(200).json({mensagem: "Produto deletado com sucesso"})
+  } catch (error) {
+    resposta.status(500).json(
+      {
+        msg: "Erro ao deletar produtos",
+        erro: error.mensagem
+      }
+    );
+  }
+})
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
