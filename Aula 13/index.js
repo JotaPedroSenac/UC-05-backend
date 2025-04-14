a84c89e63b695ef742ef6334c825002ba5356178
JOAO PEDRO DE SOUZA CORINGA <a95436285@rn.senac.local>
2025-04-14 16:52:49 -0300

    feat: feito conexao e modelagem com sequelize

--- Aula 13/index.js
+++ Aula 13/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const alunoRoutes = require('./src/modules/aluno/routes/index');
-const enderecoRoutes = require('./src/modules/endereco/routes/index');
+const sequelize = require('./src/config/configDb');
+// const alunoRoutes = require('./src/modules/aluno/routes/index');
+// const enderecoRoutes = require('./src/modules/endereco/routes/index');
 
 dotenv.config();
 
@@ -10,11 +11,17 @@ const app = express();
 
 app.use(express.json());
 // rota de aluno
-app.use(alunoRoutes); 
+// app.use(alunoRoutes); 
 // app.use("/api", professorRoutes); 
 // rota de endereco
-app.use(enderecoRoutes);
+// app.use(enderecoRoutes);
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Conectado com sucesso');
+  } catch (error) {
+    console.error('Falha na conexão com o banco', error);
+  }
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
--- Aula 13/src/modules/aluno/models/index.js
+++ Aula 13/src/modules/aluno/models/index.js
@@ -1,39 +1,54 @@
-const { pool } = require('../../../config/database');
-// model é o que está mais próximo do banco de dados
-class AlunoModel{
-    static async criar(matricula, nome, email, senha){
-        // static: não precisa ser instanciada
-        const dados = [matricula, nome, email, senha];
-        const consulta = `insert into aluno (matricula, nome, email, senha) values ($1, $2, $3, $4) returning *`;
-        const novoAluno = await pool.query(consulta, dados);
-        return novoAluno.rows;
-    }
-    static async editar(matricula, nome, email, senha){
-        const dados = [matricula, nome, email, senha];
-        const consulta = `update aluno set nome = $2, email = $3, senha = $4 where matricula = $1 returning *`;
-        const alunoAtualizado = await pool.query(consulta, dados);
-        return alunoAtualizado.rows;
-    }
-    static async listar(){
-        const consulta = `select * from aluno`;
-        const alunos = await pool.query(consulta);
-        return alunos.rows;
-    }
-    static async listarPorMatricula(matricula){
-        const dados = [matricula];
-        const consulta = `select * from aluno where matricula = $1`;
-        const aluno = await pool.query(consulta, dados);
-        return aluno.rows;
-    }
-    static async excluirPorMatricula(matricula){
-        const dados = [matricula];
-        const consulta = `delete from aluno where matricula = $1`;
-        await pool.query(consulta, dados);
-    }
-    static async excluirTodos(){
-        const consulta = `delete from aluno`;
-        await pool.query(consulta);
-    }
-}
+const { DataTypes } = require('sequelize');
+const sequelize = require('../../../config/configDb');
 
-module.exports =  AlunoModel;
+const Aluno = sequelize.define(
+  'Aluno',
+  {
+    // Model attributes are defined here
+    matricula: {
+      type: DataTypes.CHAR(5),
+      primaryKey: true
+    },
+    nome: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING(60),
+        allowNull: false,
+        unique: true,
+        validate:{
+            isEmail:{
+                msg: 'Forneça um e-mail valido!'
+            },
+            len: {
+                args: [10,60],
+                msg: 'O email deve ter no mínimo 10 caracteres e no máximo 60!'
+            }
+        }
+    },
+    senha: {
+        type: DataTypes.STRING(8),
+        allowNull: false,
+        validate:{
+            len:{
+                args: [10],
+                msg: 'A senha deve ter 10 caracteres'
+            }
+        }
+    },
+    turma_id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        // references: 
+    }
+  },
+  {
+    // Other model options go here
+    tableName: 'aluno',
+    createdAt: 'criado_em',
+    updatedAt: 'atualizado_em'
+  },
+);
+
+module.exports = Aluno;
